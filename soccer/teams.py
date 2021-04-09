bb10c3948d59c7afa1694020ebc2adb9fb296cd8
ememeti <endrit.memeti@mavs.uta.edu>
2021-04-09 00:47:44 -0500

    #13 added team abbreviations for la liga

---
+++ soccer/teams.py
@@ -0,0 +1,22 @@
+abbreviations_full = {
+    "Alavés":          "ALV",
+    "Athletic Bilbao": "ATB",
+    "Atletico Madrid": "ATM",
+    "Barcelona":       "FCB",
+    "Cádiz":           "CCF",
+    "Celta Vigo":      "CLV",
+    "Eibar":           "EIB",
+    "Elche":           "ELC",
+    "Getafe":          "GET",
+    "Granada":         "GCF",
+    "Huesca":          "HUE",
+    "Levante":         "LVT",
+    "Osasuna":         "OSA",
+    "Real Betis":      "BET",
+    "Real Madrid":     "MAD",
+    "Real Sociedad":   "SOC",
+    "Real Valladolid": "REV",
+    "Sevilla FC":      "SEV",
+    "Valencia":        "VAL",
+    "Villarreal":      "VIL"
+}
