6dca5e46c9a8e219ac2b7ef121aed3f62addeecb
ememeti <endrit.memeti@mavs.uta.edu>
2021-04-28 15:20:25 -0500

    mlb game clock fix

--- mlb/game.py
+++ mlb/game.py
@@ -75,34 +75,7 @@ class Game:
             else:
                 matchup["score"] = f"{self.away_score} - {self.home_score}"
 
-        # if self.game_stage == 'In Progress' and self.game_status == 'LIVE':
-        #     tmp = self.game_clock.split(' ')
-        #     matchup["time"], matchup["period"] = tmp[0], tmp[1]
         return matchup
-
-    # def get_clock(self, width: int) -> str:
-    #     """Get game clock and status"""
-    #     clock = self.game_clock + ' (' + self.game_status + ')'
-    #     return clock
-
-    # def is_scheduled_for(self, date: str) -> bool:
-    #     """True if this game is scheduled for the given date"""
-    #     if date.upper() in self.game_clock:
-    #         return True
-    #     else:
-    #         return False
-
-    # def is_scheduled_for_today(self) -> bool:
-    #     """True if this game is scheduled for today"""
-    #     date = utils.get_date(0)
-
-    #     # must be today
-    #     if date.upper() in self.game_clock or \
-    #             'TODAY' in self.game_clock[0:5]:
-    #         return True
-    #     elif 'Inning' in self.game_clock[-6:]:
-    #         return True
-    #     return False
 
     def is_favorite_match(self, favorites: List[str]) -> bool:
         """True if game has a team favorited by the user."""
@@ -117,12 +90,6 @@ class Scores:
     def get_scores() -> List[Tuple[str, str]]:
         """Get a list of scores/games that are on-going
                 or planned for the day (in that order)"""
-        # format = [
-        #     {'home': 'NYR', 'away': 'PIT', 'stage': 'progress', 'status': 'LIVE', 'time': '17:22', 'period': '1st', 'score': '2 - 0'},
-        #     {'home': 'MTL', 'away': 'VAN', 'stage': 'final', 'status': 'FINAL', 'clock': 'TODAY', 'score': '2 - 6'},
-        #     {'home': 'WIN', 'away': 'CGY', 'stage': '', 'status': '5:00 PM', 'clock': 'TODAY'}
-        # ]
-
         try:
             data = utils.get_JSON(constants.MLB_API)
 
@@ -143,7 +110,6 @@ class Scores:
 
                 game = Game(game_info, away_name, home_name)
 
-                # if game.is_scheduled_for_today():
                 gs.append(game.get_matchup(config.COLS)) 
 
             return gs
