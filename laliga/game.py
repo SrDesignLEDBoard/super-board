8050299dcd0c7b1c14e8e1b3423a7ec4c63902be
Amey Khairnar <ameykhairnar99@gmail.com>
2021-04-29 14:23:57 -0500

    Code clean-up 1: LaLiga

--- laliga/game.py
+++ laliga/game.py
@@ -29,16 +29,6 @@ class Game:
         self.home_name = h_name
         self.home_score = game_info['competitions'][0]['competitors'][0]['score']
 
-    def get_scoreline(self, width: int) -> Dict[str, str]:
-        """Get current score in a dict with team names and score"""
-        score = {
-            "home": self.home_name,
-            "away": self.away_name,
-            "score": f"{self.away_score} - {self.home_score}",
-            "status": self.game_status
-        }
-        return score
-
     def get_matchup(self, width: int) -> Dict[str, str]:
         """Get full names of both teams"""
         matchup = {
@@ -57,36 +47,12 @@ class Game:
         matchup['period'] = 'H' + str(self.game_period)
         return matchup
 
-    def get_clock(self, width: int) -> str:
-        """Get game clock and status"""
-        clock = self.game_clock + ' (' + self.game_status + ')'
-        return clock
-
-    def is_scheduled_for(self, date: str) -> bool:
-        """True if this game is scheduled for the given date"""
-        if date.upper() in self.game_clock:
-            return True
-        else:
-            return False
-
-    def is_favorite_match(self, favorites: List[str]) -> bool:
-        """True if game has a team favorited by the user."""
-        for team in favorites:
-            if team == self.home_name or team == self.away_name:
-                return True
-        return False
-
 
 class Scores:
     @staticmethod
     def get_scores() -> List[Tuple[str, str]]:
         """Get a list of scores/games that are on-going
                 or planned for the day (in that order)"""
-        # format = [
-        #     {'home': 'NYR', 'away': 'PIT', 'stage': 'progress', 'status': 'LIVE', 'time': '17:22', 'period': '1st', 'score': '2 - 0'},
-        #     {'home': 'MTL', 'away': 'VAN', 'stage': 'final', 'status': 'FINAL', 'clock': 'TODAY', 'score': '2 - 6'},
-        #     {'home': 'WIN', 'away': 'CGY', 'stage': '', 'status': '5:00 PM', 'clock': 'TODAY'}
-        # ]
 
         try:
             data = utils.get_JSON(constants.LALIGA_API)
